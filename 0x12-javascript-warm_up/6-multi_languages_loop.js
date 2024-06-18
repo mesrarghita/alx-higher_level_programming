6fadda2f26d7d566b604caf73452cc4e6805803c
mesrarghita <ghita1mesrar@gmail.com>
2024-06-18 13:40:08 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/*
+ *  a script that prints 3 lines
+ */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
