3dc26459238f3e4fe029a698fed86c599d82767c
mesrarghita <ghita1mesrar@gmail.com>
2024-07-30 09:17:58 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,13 +2,29 @@
 
 const request = require('request');
 const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: ./5-request_store.js <URL> <file path>');
+  process.exit(1);
+}
+
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch page, status code: ${response.statusCode}`);
+    process.exit(1);
+  }
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(`Failed to write to file: ${err.message}`);
+      process.exit(1);
+    }
+    console.log(`Content successfully written to ${filePath}`);
+  });
 });
